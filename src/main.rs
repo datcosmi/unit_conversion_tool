0f9d999629d0414b315f78d460e1fbcbbe1dfd76
Ivan Garcia <garcia.cli@pm.me>
2025-12-23 01:44:45 -0600

    add kelvin & optimizations

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,59 @@ use std::io;
 
 use owo_colors::OwoColorize;
 
-enum Conversion {
-    FahrenheitToCelsius,
-    CelsiusToFahrenheit,
-}
-
+#[derive(Copy, Clone)]
 enum Unit {
     Fahrenheit,
     Celsius,
+    Kelvin,
 }
 
+#[derive(Copy, Clone)]
 struct Temperature {
     value: f64,
     unit: Unit,
 }
 
-impl Conversion {
-    fn label(&self) -> &'static str {
+impl Temperature {
+    fn new(value: f64, unit: Unit) -> Self {
+        Self { value, unit }
+    }
+}
+
+impl Unit {
+    fn to_kelvin(tmp: Temperature) -> Temperature {
+        let value = match tmp.unit {
+            Self::Celsius => tmp.value + 273.15,
+            Self::Fahrenheit => (tmp.value - 32.0) * (5.0 / 9.0) + 273.15,
+            Self::Kelvin => tmp.value,
+        };
+
+        Temperature::new(value, Unit::Kelvin)
+    }
+
+    fn from_kelvin(tmp: Temperature, target: Unit) -> Temperature {
+        let value = match target {
+            Self::Celsius => tmp.value - 273.15,
+            Self::Fahrenheit => (tmp.value - 273.15) * (9.0 / 5.0) + 32.0,
+            Self::Kelvin => tmp.value,
+        };
+
+        Temperature::new(value, target)
+    }
+
+    fn name(&self) -> &'static str {
         match self {
-            Conversion::FahrenheitToCelsius => "Fahrenheit to Celsius",
-            Conversion::CelsiusToFahrenheit => "Celsius to Fahrenheit",
+            Self::Fahrenheit => "Fahrenheit",
+            Self::Celsius => "Celsius",
+            Self::Kelvin => "Kelvin",
         }
     }
 
-    fn unit(&self) -> Unit {
+    fn symbol(&self) -> &'static str {
         match self {
-            Conversion::FahrenheitToCelsius => Unit::Fahrenheit,
-            Conversion::CelsiusToFahrenheit => Unit::Celsius,
-        }
-    }
-
-    fn apply(&self, t: f64) -> Temperature {
-        match self {
-            Conversion::FahrenheitToCelsius => convert_to_celsius(t),
-            Conversion::CelsiusToFahrenheit => convert_to_fahrenheit(t),
+            Self::Fahrenheit => "°F",
+            Self::Celsius => "°C",
+            Self::Kelvin => "°K",
         }
     }
 }
@@ -44,42 +63,53 @@ impl Conversion {
 // Format to print
 impl fmt::Display for Temperature {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:.2} {}", self.value, self.unit)
+        write!(f, "{:.2} {}", self.value, self.unit.symbol())
     }
 }
 
-impl fmt::Display for Unit {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Unit::Fahrenheit => write!(f, "°F"),
-            Unit::Celsius => write!(f, "°C"),
-        }
-    }
-}
+// impl fmt::Display for Unit {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         match self {
+//             Unit::Fahrenheit => write!(f, "°F"),
+//             Unit::Celsius => write!(f, "°C"),
+//             Unit::Kelvin => write!(f, "°K"),
+//         }
+//     }
+// }
 
 fn main() {
-    println!("{}", "\nTEMPERATURE CONVERSION HELPER\n".bold().purple());
+    println!("{}", "\n------ ^^ Temperature Conversion Tool ^^ ------\n".bold().purple());
 
-    let conversion = ask_conversion();
+    let source = ask_source();
 
-    println!("\nYou chose: {}", conversion.label().bold().green());
+    println!("\n--- {} {} {} ---", "You chose:".bold().blue(), source.name().bold().green(), source.symbol().bold().green());
 
-    let temperature = ask_temperature(conversion.unit());
+    let target = ask_target();
 
-    let result = conversion.apply(temperature.value);
+    println!("\n--- {} {} {} ---", "You chose:".bold().blue(), target.name().bold().green(), target.symbol().bold().green());
 
+    let temperature = ask_temperature(source);
+    let canon_unit = Unit::to_kelvin(temperature);
+    let result = Unit::from_kelvin(canon_unit, target);
+
+    println!("\n          {}", "Awesome!!! :D".bold().purple());
     println!(
-        "\n{} equals to {}",
+        "\n--- {} equals to {}! ^^ ---",
         temperature.bold().green(),
         result.bold().green()
     );
+    println!("\n    {}", "Thank you for using this tool!".bold().purple());
 }
 
-fn ask_conversion() -> Conversion {
+fn ask_source() -> Unit {
     loop {
-        println!("Choose conversion: {}", "(Type number only)".bold().blue());
-        println!("{} Fahrenheit to Celsius", "(1)".bold().green());
-        println!("{} Celsius to Fahrenheit", "(2)".bold().green());
+        println!(
+            "What will we convert today? :D {}",
+            "(Type number only)".bold().blue()
+        );
+        println!("{} Fahrenheit {}", "(1)".bold().green(), "°F".blue());
+        println!("{} Celsius {}", "(2)".bold().green(), "°C".blue());
+        println!("{} Kelvin {}", "(3)".bold().green(), "°K".blue());
 
         let mut input = String::new();
 
@@ -88,8 +118,39 @@ fn ask_conversion() -> Conversion {
             .expect("Failed to read line");
 
         match input.trim() {
-            "1" => return Conversion::FahrenheitToCelsius,
-            "2" => return Conversion::CelsiusToFahrenheit,
+            "1" => return Unit::Fahrenheit,
+            "2" => return Unit::Celsius,
+            "3" => return Unit::Kelvin,
+            _ => println!(
+                "\n{}\n",
+                "Input was either a string or an invalid number. Try again."
+                    .bold()
+                    .red()
+            ),
+        }
+    }
+}
+
+fn ask_target() -> Unit {
+    loop {
+        println!(
+            "And what will we convert this into? :o {}",
+            "(Type number only)".bold().blue()
+        );
+        println!("{} Fahrenheit {}", "(1)".bold().green(), "°F".blue());
+        println!("{} Celsius {}", "(2)".bold().green(), "°C".blue());
+        println!("{} Kelvin {}", "(3)".bold().green(), "°K".blue());
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        match input.trim() {
+            "1" => return Unit::Fahrenheit,
+            "2" => return Unit::Celsius,
+            "3" => return Unit::Kelvin,
             _ => println!(
                 "\n{}\n",
                 "Input was either a string or an invalid number. Try again."
@@ -124,25 +185,42 @@ fn ask_temperature(u: Unit) -> Temperature {
                 continue;
             }
         };
-        return create_temperature(value, u);
+        return Temperature::new(value, u);
     }
 }
 
-fn convert_to_celsius(f: f64) -> Temperature {
-    let value: f64 = (f - 32.0) * 5.0 / 9.0;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    return create_temperature(value, Unit::Celsius);
-}
+    #[test]
+    fn from_celsius_to_kelvin_works() {
+        let result = Unit::to_kelvin(Temperature::new(13.0, Unit::Celsius)).value;
 
-fn convert_to_fahrenheit(c: f64) -> Temperature {
-    let value: f64 = (c * (9.0 / 5.0)) + 32.0;
+        assert!((result - 286.15).abs() < 0.01);
+    }
 
-    return create_temperature(value, Unit::Fahrenheit);
-}
+    #[test]
+    fn from_fahrenheit_to_kelvin_works() {
+        let result = Unit::to_kelvin(Temperature::new(13.0, Unit::Fahrenheit)).value;
 
-fn create_temperature(v: f64, u: Unit) -> Temperature {
-    Temperature {
-        value: v,
-        unit: u,
+        assert!((result - 262.594).abs() < 0.01);
+    }
+
+    #[test]
+    fn from_kelvin_to_celsius_works() {
+        let result =
+            Unit::from_kelvin(Temperature::new(286.15, Unit::Kelvin), Unit::Celsius).value;
+
+        assert!((result - 13.0).abs() < 0.01);
+    }
+
+    #[test]
+    fn from_kelvin_to_fahrenheit_works() {
+        let result =
+            Unit::from_kelvin(Temperature::new(262.594, Unit::Kelvin), Unit::Fahrenheit)
+                .value;
+
+        assert!((result - 13.0).abs() < 0.01);
     }
 }
