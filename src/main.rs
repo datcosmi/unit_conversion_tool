f6213809fee2a31906bf7ca02a268b955260c576
Ivan Garcia <garcia.cli@pm.me>
2025-12-22 20:17:56 -0600

    owo_colors and pretty output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::io;
 use std::fmt;
+use std::io;
+
+use owo_colors::OwoColorize;
 
 enum Conversion {
     FahrenheitToCelsius,
@@ -32,9 +34,17 @@ impl fmt::Display for Unit {
 }
 
 fn main() {
-    println!("Convert your temperatures!");
+    println!("{}", "\nTEMPERATURE CONVERTION HELPER\n".bold().purple());
 
     let conversion = ask_conversion();
+
+    let choice = match conversion {
+        Conversion::FahrenheitToCelsius => "Fahrenheit to Celsius",
+        Conversion::CelsiusToFahrenheit => "Celsius to Fahrenheit",
+    };
+
+    println!("\nYou chose: {}", choice.bold().green());
+
     let temperature = ask_temperature();
 
     let result = match conversion {
@@ -42,16 +52,25 @@ fn main() {
         Conversion::CelsiusToFahrenheit => convert_to_fahrenheit(temperature),
     };
 
-    println!("Result: {result}");
+    let temperature = Temperature {
+        value: temperature,
+        unit: match conversion {
+            Conversion::FahrenheitToCelsius => Unit::Fahrenheit,
+            Conversion::CelsiusToFahrenheit => Unit::Celsius,
+        }
+    };
+
+    println!("\n{} equals to {}", temperature.bold().green(), result.bold().green());
 }
 
 fn ask_conversion() -> Conversion {
     loop {
-        println!("Choose conversion:");
-        println!("(1) Fahrenheit to Celsius");
-        println!("(2) Celsius to Fahrenheit");
+        println!("Choose conversion: {}", "(Type number only)".bold().blue());
+        println!("{} Fahrenheit to Celsius", "(1)".bold().green());
+        println!("{} Celsius to Fahrenheit", "(2)".bold().green());
 
         let mut input = String::new();
+
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
@@ -59,37 +78,43 @@ fn ask_conversion() -> Conversion {
         match input.trim() {
             "1" => return Conversion::FahrenheitToCelsius,
             "2" => return Conversion::CelsiusToFahrenheit,
-            _ => println!("Invalid input. Try again \n"),
+            _ => println!("\n{}\n", "Input was either a string or an invalid number. Try again.".bold().red()),
         }
     }
 }
 
 fn ask_temperature() -> f64 {
     loop {
-        println!("Input the temperature value: ");
+        println!("\nInput the temperature value: {}", "(You can use decimals too!)".bold().blue());
 
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
-        
+
         match input.trim().parse::<f64>() {
             Ok(num) => return num,
-            Err(_) => println!("Invalid number. Try again \n"),
+            Err(_) => println!("\n{}", "Input was either a string or an invalid number. Try again.".bold().red()),
         }
     }
 }
 
 fn convert_to_celsius(f: f64) -> Temperature {
     let value: f64 = (f - 32.0) * 5.0 / 9.0;
-    let temperature = Temperature {value: value, unit: Unit::Celsius};
+    let temperature = Temperature {
+        value: value,
+        unit: Unit::Celsius,
+    };
 
     return temperature;
 }
 
 fn convert_to_fahrenheit(c: f64) -> Temperature {
     let value: f64 = (c * (9.0 / 5.0)) + 32.0;
-    let temperature = Temperature {value: value, unit: Unit::Fahrenheit};
+    let temperature = Temperature {
+        value: value,
+        unit: Unit::Fahrenheit,
+    };
 
     return temperature;
 }
